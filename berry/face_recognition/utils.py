3f1cc319fb29c2ada6b83c9508954eed67b96454
Raihan Tanvir <rtanvir@wisc.edu>
2024-10-21 20:22:12 -0500

    Added pair generation code for SNN, uses positive and negative images

--- berry/face_recognition/utils.py
+++ berry/face_recognition/utils.py
@@ -1,4 +1,5 @@
 import config
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,7 +8,34 @@ def euclidean_distance(vector1, vector2):
     return distance
 
 def create_pairs(dataset, pairs=10):
-    data = []
+    positive_pairs = []
+    for img1, img2 in itertools.combinations(config.POSITIVE_DATASET_PATH, 2):
+        pair = (img1, img2, 1)
+        positive_pairs.append(pair)
+    seen_pairs = {}
+    unique_positive_pairs = []
+    for img1, img2, label in positive_pairs:
+        pair_id = tuple(sorted([id(img1), id(img2)]))
+        if pair_id not in seen_pairs:
+            seen_pairs.add(pair_id)
+            unique_positive_pairs.append((img1, img2, label))
+    positive_pairs = unique_positive_pairs
+        
+    negative_pairs = []
+    for img1 in config.POSITIVE_DATASET_PATH:
+        for img2 in config.NEGATIVE_DATASET_PATH:
+            pair = (img1, img2, 0)
+            negative_pairs.append(pair)
+    seen_pairs = {}
+    unique_negative_pairs = []
+    for img1, img2, label in negative_pairs:
+        pair_id = tuple(sorted([id(img1), id(img2)]))
+        if pair_id not in seen_pairs:
+            seen_pairs.add(pair_id)
+            unique_negative_pairs.append((img1, img2, label))
+    negative_pairs = unique_negative_pairs
+            
+    return positive_pairs, negative_pairs
 
 def plot_training(training_history, path):
     plt.style.use("ggplot")
